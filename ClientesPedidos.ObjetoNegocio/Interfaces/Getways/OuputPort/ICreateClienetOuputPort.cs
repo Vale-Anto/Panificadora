f13151f4e8a57181e4f23982a4b101eedbdbd53c
Vale-Anto <antonelafranco7@gmail.com>
2023-11-13 18:20:28 -0300

    nuevas clases 13/11

---
+++ ClientesPedidos.ObjetoNegocio/Interfaces/Getways/OuputPort/ICreateClienetOuputPort.cs
@@ -0,0 +1,17 @@
+﻿using ClientesPedidos.ObjetoNegocio.Wrappers.Cliente;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientesPedidos.ObjetoNegocio.Interfaces.Getways.OuputPort
+{
+    public interface ICreateClienetOuputPort
+    {
+
+        Task Handle(CrearClienteResponse cliente);
+
+
+    }
+}
