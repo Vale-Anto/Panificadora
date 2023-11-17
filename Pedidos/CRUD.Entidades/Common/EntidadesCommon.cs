c4c3c01180e721539c2c200ff7df0b061914e943
OrganicGarden <alvarezjeldesvalentina@gmail.com>
2023-11-17 12:15:59 -0300

    subir mi brunch
    ultimos cambios - subir

--- Pedidos/CRUD.Entidades/Common/EntidadesCommon.cs
+++ Pedidos/CRUD.Entidades/Common/EntidadesCommon.cs
@@ -10,7 +10,7 @@
         public DateTime CreatedDate { get; set; } = DateTime.UtcNow; //fecha de creación 
         public int UpdatedBy { get; set; } //s un número entero que representa el identificador del
                                            //usuario que actualizó la clase por última vez
-        public DateTime UpdatedDate { get; set; } = DateTime.UtcNow; //fecha actualizada
+        public DateTime UpdatedDate { get; set; } = DateTime.UtcNow; //fecha actualizadas
 
         public bool IsDeleted { get; set; } = false;
 
